e83949e800f0c1026d776af45440a3d0395bc7d1
MoJair <1639269322@qq.com>
2023-08-30 02:06:44 +0800

    临时注释

--- Flexibility_Plugins/js/popup.js
+++ Flexibility_Plugins/js/popup.js
@@ -2,8 +2,8 @@ console.log("打开了popup页面");
 
 window.onload = function startTime(){
     //getNowTime();
-    const bg = chrome.extension.getBackgroundPage();
-    var bg1 = chrome.runtime.getBackgroundPage();
+    //const bg = chrome.extension.getBackgroundPage();
+    //var bg1 = chrome.runtime.getBackgroundPage();
     //bg.getNowTime();
     //getCountDown();
 console.log(bg1)
